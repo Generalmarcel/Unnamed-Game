572e47d0fcec12794d0b7115701c42bc674db68e
赤城 <42133415+MikuAdmin@users.noreply.github.com>
2019-05-23 10:06:38 -0700

    Create camps.h

---
+++ src/characters/headers/camps.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#ifndef CAMPS_H
+#define CAMPS_H
+
+namespace camps
+{
+	enum navys {
+		CCCP,
+		CNS,
+		DKM,
+		FGS,
+		FNFL,
+		FSNF,
+		HTMS,
+		HMAS,
+		HMS,
+		IJN,
+		RM,
+		ROC,
+		USS
+	};
+
+	enum nations {
+		GERMANY,
+		FRANCE,
+		ITALY,
+		JAPAN,
+		CHINA,
+		USA,
+		USSR,
+		THAILAND,
+		AUSTRALIA,
+		FEDERAL_COSMIC_ARMED_FORCES
+	};
+}
+
+#endif // !CAMPS_H
